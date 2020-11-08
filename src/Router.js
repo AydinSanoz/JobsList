da83eb204c9c1af628a85d34f379d55f51149ed3
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-08 23:25:51 +0300

    JobListApp started

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const Router = () =>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Welcome on Board</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Router;
---
+++ src/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+
+export topicItem = StyleSheet.create({
+    container :{
+        //null
+    },
+    text:{
+       // null
+    }
+
+})
+
+export jobItem = StyleSheet.create({
+    container:{
+        //null
+    },
+    text:{
+        //null
+    }
+})
